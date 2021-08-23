0109d8e3e51ef84ca36f637fae834ac36169b8d9
Sunnesoft <sunnesoft@gmail.com>
2021-08-23 20:55:41 +0000

    add tests

---
+++ test/test_gw_preprocess.py
@@ -0,0 +1,82 @@
+from unittest import TestCase
+
+
+class TestGwTimeseries(TestCase):
+    def test_load(self):
+        self.fail()
+
+    def test_save(self):
+        self.fail()
+
+    def test_plot(self):
+        self.fail()
+
+    def test_crop(self):
+        self.fail()
+
+    def test_copy(self):
+        self.fail()
+
+    def test_apply_window(self):
+        self.fail()
+
+    def test_whiten(self):
+        self.fail()
+
+    def test_psd(self):
+        self.fail()
+
+    def test_filter(self):
+        self.fail()
+
+    def test_value(self):
+        self.fail()
+
+    def test_time(self):
+        self.fail()
+
+    def test_sample_rate(self):
+        self.fail()
+
+    def test_duration(self):
+        self.fail()
+
+    def test_time_step(self):
+        self.fail()
+
+    def test_name(self):
+        self.fail()
+
+
+class TestGwSpectrogram(TestCase):
+    def test_cqt(self):
+        self.fail()
+
+    def test_cwt(self):
+        self.fail()
+
+    def test_normalize(self):
+        self.fail()
+
+    def test_show_value(self):
+        self.fail()
+
+    def test_value(self):
+        self.fail()
+
+    def test_time(self):
+        self.fail()
+
+    def test_frequencies(self):
+        self.fail()
+
+    def test_save(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_interpolate_cqt_data(self):
+        self.fail()
+
+    def test_qsearch(self):
+        self.fail()
